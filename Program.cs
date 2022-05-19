89051b29534d1f8473b9e4698b83f7daa96a8d8b
ima2022 <104914994+ima2022@users.noreply.github.com>
2022-05-19 23:55:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Traking_Student_Grades_in_Maths;
+
+ string StudentName = "Eman";
+ char Grade = 'C';
+ int Group = 3;
+
+Student student = new(StudentName, Grade, Group);
+
+student.GetStudentName();
+student.GetStudentGrade();
+student.GetGroupValue();
+
+if (Grade == 'A' || Grade == 'B')
+{
+    student.UpgradeStudentGrade(Grade);
+}
+
+if (Grade <= 'C')
+{
+    student.DownGrade(Grade);
+}
+
---
+++ Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traking_Student_Grades_in_Maths
+{
+    public class Student
+    {
+        private string StudentName { get; set; }
+        private char Grade { get; set; }
+        private int Group { get; set; }    
+
+        public Student(string _studentname, char _grade, int _group)
+        {
+            StudentName = _studentname;
+            Grade = _grade; 
+            Group = _group; 
+        }
+
+        public void GetStudentName()
+        {
+            Console.WriteLine($"Student Name is {StudentName}");
+        }
+
+        public void GetStudentGrade()
+        {
+            Console.WriteLine($"Student Grade is {Grade}");
+        }
+       
+        public void GetGroupValue()
+        {
+            Console.WriteLine($"Student Group Value is {Group}");
+        }
+        public void UpgradeStudentGrade(char Grade)
+        {
+            if (Grade == 'B' )
+            {
+                Grade = 'A'; 
+            }
+
+            Console.WriteLine($"The Student Grade has Upgraded to {Grade}");
+        }
+        public void DownGrade(char Grade)
+        {
+            if (Grade == 'C')
+            {
+                Grade = 'D';
+            }
+            if (Grade == 'D')
+            {
+                Grade = 'F';
+            }
+
+            Console.WriteLine($"The Student Grade has downgraded to {Grade}");
+        }
+    }
+
+    
+
+}
